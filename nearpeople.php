f1888a3d9937965291db8dce8fa023bd6b4e299a
Rupesh209 <51116386+Rupesh209@users.noreply.github.com>
2020-08-30 13:52:46 +0530

    Update nearpeople.php

--- nearpeople.php
+++ nearpeople.php
@@ -17,25 +17,7 @@ $link = mysqli_connect($servername, $username, $password, $dbname);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-function twopoints_on_earth($latitudeFrom, $longitudeFrom, $latitudeTo,  $longitudeTo) 
-      {
-          $long1 = deg2rad($longitudeFrom); 
-          $long2 = deg2rad($longitudeTo); 
-          $lat1 = deg2rad($latitudeFrom); 
-          $lat2 = deg2rad($latitudeTo); 
-              
-          
-          $dlong = $long2 - $long1; 
-          $dlati = $lat2 - $lat1; 
-              
-          $val = pow(sin($dlati/2),2)+cos($lat1)*cos($lat2)*pow(sin($dlong/2),2); 
-              
-          $res = 2 * asin(sqrt($val)); 
-              
-          $radius = 3958.756; 
-              
-          return (($res*$radius) * 1.60934); 
-      } 
+
 $sql = "SELECT * FROM Userdatabase WHERE username !=$username2 AND 1>=(twopoints_on_earth($latitude, $longitude,latitude, longitude))";
 if($result = mysqli_query($link, $sql)){
     if(mysqli_num_rows($result) > 0){
